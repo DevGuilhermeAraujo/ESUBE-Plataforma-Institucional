8fa8abc3f9114295ec42c2a31705b2f1a19bab04
GabrielGarcia10 <142952247+GabrielGarcia10@users.noreply.github.com>
2023-10-19 16:14:54 -0300

    Banco de dados v0.2
    Fix arquivo

--- db/escola_db.sql
+++ db/escola_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19/10/2023 às 21:03
+-- Tempo de geração: 19/10/2023 às 21:14
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `escola_db`
 --
+CREATE DATABASE IF NOT EXISTS `escola_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `escola_db`;
 
 -- --------------------------------------------------------
 
