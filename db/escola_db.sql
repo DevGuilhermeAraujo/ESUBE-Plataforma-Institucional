f5f64c439508daa612c89324d529a0c5284aec67
GabrielGarcia10 <142952247+GabrielGarcia10@users.noreply.github.com>
2023-10-19 16:04:56 -0300

    Banco de Dados v0.2
    Atualização do banco.

--- db/escola_db.sql
+++ db/escola_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 18/10/2023 às 03:54
+-- Tempo de geração: 19/10/2023 às 21:03
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `ra` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `aulas`
+--
+
+CREATE TABLE `aulas` (
+  `cod` int(10) NOT NULL,
+  `materia` int(5) NOT NULL,
+  `idProfessor` int(10) NOT NULL,
+  `nomeSala` varchar(15) DEFAULT NULL,
+  `assunto` varchar(25) DEFAULT NULL,
+  `data_hora` date DEFAULT NULL,
+  `status` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `cursos`
 --
 
@@ -35,14 +61,31 @@ CREATE TABLE `cursos` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `gerentes`
+--
+
+CREATE TABLE `gerentes` (
+  `id` int(10) NOT NULL,
+  `salario` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `gerentes`
+--
+
+INSERT INTO `gerentes` (`id`, `salario`) VALUES
+(1111111, 0.00);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `materias`
 --
 
 CREATE TABLE `materias` (
   `cod` int(5) NOT NULL,
   `codCurso` int(5) NOT NULL,
-  `nomeMateria` varchar(30) NOT NULL,
-  `nota` int(3) DEFAULT NULL
+  `nomeMateria` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -54,19 +97,34 @@ CREATE TABLE `materias` (
 CREATE TABLE `matriculas` (
   `cod` int(10) NOT NULL,
   `raAluno` int(10) NOT NULL,
-  `codMaterial` int(5) NOT NULL,
+  `codCurso` int(5) NOT NULL,
+  `mensalidade` decimal(5,2) DEFAULT NULL,
+  `dataMAtricula` date DEFAULT NULL,
   `status` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `permissoes`
+-- Estrutura para tabela `notas`
 --
 
-CREATE TABLE `permissoes` (
-  `id` int(5) NOT NULL,
-  `nome` varchar(15) NOT NULL
+CREATE TABLE `notas` (
+  `matricula` int(10) NOT NULL,
+  `materia` int(5) DEFAULT NULL,
+  `nota` int(3) DEFAULT NULL,
+  `status` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `professores`
+--
+
+CREATE TABLE `professores` (
+  `id` int(10) NOT NULL,
+  `salario` decimal(5,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -76,13 +134,9 @@ CREATE TABLE `permissoes` (
 --
 
 CREATE TABLE `turmas` (
-  `cod` int(10) NOT NULL,
-  `materia` int(5) NOT NULL,
-  `raAluno` int(10) NOT NULL,
-  `idProfessor` int(10) NOT NULL,
-  `nomeSala` varchar(15) DEFAULT NULL,
-  `data_hora` date DEFAULT NULL,
-  `status` varchar(15) DEFAULT NULL
+  `aula` int(10) NOT NULL,
+  `aluno` int(10) NOT NULL,
+  `presenca` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -92,25 +146,51 @@ CREATE TABLE `turmas` (
 --
 
 CREATE TABLE `usuarios` (
-  `ra` int(10) NOT NULL,
-  `idPermissao` int(5) NOT NULL,
-  `nome` varchar(15) NOT NULL,
-  `sexo` int(1) NOT NULL,
+  `id` int(10) NOT NULL,
+  `nome` varchar(50) NOT NULL,
   `CPF` varchar(14) DEFAULT NULL,
+  `sexo` int(1) NOT NULL,
   `dataNasc` date DEFAULT NULL,
   `email` varchar(30) DEFAULT NULL,
   `senha` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `CPF`, `sexo`, `dataNasc`, `email`, `senha`) VALUES
+(1111111, 'Administrador', NULL, 0, NULL, NULL, 'admin');
+
+--
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `alunos`
+--
+ALTER TABLE `alunos`
+  ADD KEY `ra` (`ra`);
+
+--
+-- Índices de tabela `aulas`
+--
+ALTER TABLE `aulas`
+  ADD PRIMARY KEY (`cod`),
+  ADD KEY `materia` (`materia`),
+  ADD KEY `idProfessor` (`idProfessor`);
 
 --
 -- Índices de tabela `cursos`
 --
 ALTER TABLE `cursos`
   ADD PRIMARY KEY (`cod`);
+
+--
+-- Índices de tabela `gerentes`
+--
+ALTER TABLE `gerentes`
+  ADD KEY `id` (`id`);
 
 --
 -- Índices de tabela `materias`
@@ -125,33 +205,90 @@ ALTER TABLE `materias`
 ALTER TABLE `matriculas`
   ADD PRIMARY KEY (`cod`),
   ADD KEY `raAluno` (`raAluno`),
-  ADD KEY `codMaterial` (`codMaterial`);
+  ADD KEY `codCurso` (`codCurso`);
 
 --
--- Índices de tabela `permissoes`
+-- Índices de tabela `notas`
 --
-ALTER TABLE `permissoes`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `notas`
+  ADD KEY `matricula` (`matricula`),
+  ADD KEY `materia` (`materia`);
+
+--
+-- Índices de tabela `professores`
+--
+ALTER TABLE `professores`
+  ADD KEY `id` (`id`);
 
 --
 -- Índices de tabela `turmas`
 --
 ALTER TABLE `turmas`
-  ADD PRIMARY KEY (`cod`),
-  ADD KEY `materia` (`materia`),
-  ADD KEY `raAluno` (`raAluno`),
-  ADD KEY `idProfessor` (`idProfessor`);
+  ADD KEY `aula` (`aula`),
+  ADD KEY `aluno` (`aluno`);
 
 --
 -- Índices de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`ra`),
-  ADD KEY `idPermissao` (`idPermissao`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `aulas`
+--
+ALTER TABLE `aulas`
+  MODIFY `cod` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `cod` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `materias`
+--
+ALTER TABLE `materias`
+  MODIFY `cod` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `matriculas`
+--
+ALTER TABLE `matriculas`
+  MODIFY `cod` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1111112;
 
 --
 -- Restrições para tabelas despejadas
 --
+
+--
+-- Restrições para tabelas `alunos`
+--
+ALTER TABLE `alunos`
+  ADD CONSTRAINT `alunos_ibfk_1` FOREIGN KEY (`ra`) REFERENCES `usuarios` (`id`);
+
+--
+-- Restrições para tabelas `aulas`
+--
+ALTER TABLE `aulas`
+  ADD CONSTRAINT `aulas_ibfk_1` FOREIGN KEY (`materia`) REFERENCES `materias` (`cod`),
+  ADD CONSTRAINT `aulas_ibfk_2` FOREIGN KEY (`idProfessor`) REFERENCES `professores` (`id`);
+
+--
+-- Restrições para tabelas `gerentes`
+--
+ALTER TABLE `gerentes`
+  ADD CONSTRAINT `gerentes_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`);
 
 --
 -- Restrições para tabelas `materias`
@@ -163,22 +300,28 @@ ALTER TABLE `materias`
 -- Restrições para tabelas `matriculas`
 --
 ALTER TABLE `matriculas`
-  ADD CONSTRAINT `matriculas_ibfk_1` FOREIGN KEY (`raAluno`) REFERENCES `usuarios` (`ra`),
-  ADD CONSTRAINT `matriculas_ibfk_2` FOREIGN KEY (`codMaterial`) REFERENCES `materias` (`cod`);
+  ADD CONSTRAINT `matriculas_ibfk_1` FOREIGN KEY (`raAluno`) REFERENCES `alunos` (`ra`),
+  ADD CONSTRAINT `matriculas_ibfk_2` FOREIGN KEY (`codCurso`) REFERENCES `cursos` (`cod`);
+
+--
+-- Restrições para tabelas `notas`
+--
+ALTER TABLE `notas`
+  ADD CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`matricula`) REFERENCES `matriculas` (`cod`),
+  ADD CONSTRAINT `notas_ibfk_2` FOREIGN KEY (`materia`) REFERENCES `materias` (`cod`);
+
+--
+-- Restrições para tabelas `professores`
+--
+ALTER TABLE `professores`
+  ADD CONSTRAINT `professores_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`);
 
 --
 -- Restrições para tabelas `turmas`
 --
 ALTER TABLE `turmas`
-  ADD CONSTRAINT `turmas_ibfk_1` FOREIGN KEY (`materia`) REFERENCES `materias` (`cod`),
-  ADD CONSTRAINT `turmas_ibfk_2` FOREIGN KEY (`raAluno`) REFERENCES `usuarios` (`ra`),
-  ADD CONSTRAINT `turmas_ibfk_3` FOREIGN KEY (`idProfessor`) REFERENCES `usuarios` (`ra`);
-
---
--- Restrições para tabelas `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`idPermissao`) REFERENCES `permissoes` (`id`);
+  ADD CONSTRAINT `turmas_ibfk_1` FOREIGN KEY (`aula`) REFERENCES `aulas` (`cod`),
+  ADD CONSTRAINT `turmas_ibfk_2` FOREIGN KEY (`aluno`) REFERENCES `alunos` (`ra`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
