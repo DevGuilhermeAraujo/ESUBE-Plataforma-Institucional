2f1700cb1951860085009bd8e7bec29fbdc6211f
Matheus <matheusoficial1411@gmail.com>
2023-10-23 14:14:47 -0300

    versão front end com cadastro de notas

---
+++ Gerente/Cadastrados/alunos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alunos</title>
+    <link rel="stylesheet" href="../../index.css">
+</head>
+<body>
+<div id="exib"> 
+    <p><!--Aqui deverá ficar uma lista com todos os alunos cadastrados--></p>
+    </div>
+</body>
+</html>
