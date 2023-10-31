0097d136d1f7bdd3a4ed96810b976ab756bc448c
GabrielGarcia10 <garcia.gabrielrodrigues1@gmail.com>
2023-10-31 15:54:08 -0300

    Validação de conexão com o banco

--- Gerente/inicioGerente.php
+++ Gerente/inicioGerente.php
@@ -1,17 +1,24 @@
 <?php
 //Deve estar presente em todas as paginas
-
 include_once '../BackEnd/sessao.php';
+//Deve estar presente se o login for obrigatório 
+requiredLogin(PERMISSION_GERENTE);
+
 require_once('../BackEnd/conexao.php');
 $db = new Conexao();
-$result = $db->executar("SELECT COUNT(*) FROM view_professores;");
-$quantProf = $result;
-$result = $db->executar("SELECT COUNT(*) FROM view_alunos;");
-$quantAlunos = $result;
-$result = $db->executar("SELECT COUNT(*) FROM turmas;");
-$quantTurmas = $result;
-//Deve estar presente se o login for obrigatório 
-//requiredLogin(); <- Desativado até a conexão do banco
+if ($db->errorCode != 0) {
+    echo '<span class="msgN">
+            Falha ao conectar com a base de dados, Tente novamente mais tarde.<br>Se o problema persistir, por favor entre em contato com o adminstrador do sistema.
+        </span>';
+    exit();
+}
+    $result = $db->executar("SELECT COUNT(*) FROM view_professores;");
+    $quantProf = $result;
+    $result = $db->executar("SELECT COUNT(*) FROM view_alunos;");
+    $quantAlunos = $result;
+    $result = $db->executar("SELECT COUNT(*) FROM turmas;");
+    $quantTurmas = $result;
+
 ?>
 
 <!DOCTYPE html>
