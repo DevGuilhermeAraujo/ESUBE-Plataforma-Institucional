eed7007f5c94fd3d0ad6c7d2bddb6bcbb47eb7b0
Matheus <matheusoficial1411@gmail.com>
2023-11-01 10:07:29 -0300

    front.add

--- Gerente/cadUser.php
+++ Gerente/cadUser.php
@@ -1,7 +1,6 @@
 <?php
 require_once '../BackEnd/sessao.php';
 requiredLogin(PERMISSION_GERENTE);
-
 require_once('../BackEnd/conexao.php');
 $db = new Conexao();
 if ($db->errorCode == 0) {
@@ -16,7 +15,6 @@ if ($db->errorCode == 0) {
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,53 +23,33 @@ if ($db->errorCode == 0) {
     <link rel="stylesheet" href="cadastros.css">
     <script src="../BackEnd/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
 </head>
-
 <body>
     <form method="POST" action="../BackEnd/processCadastro.php" onsubmit="return validateForm()" novalidate>
         <h2>Cadastro</h2>
-
         <?php
         //Validação Banco
         if ($db->errorCode != 0) {
-            msg(2,"Falha ao conectar com a base de dados, Tente novamente mais tarde.<br>Se o problema persistir, por favor entre em contato com o adminstrador do sistema.");
+            msg(2, "Falha ao conectar com a base de dados, Tente novamente mais tarde.<br>Se o problema persistir, por favor entre em contato com o adminstrador do sistema.");
 
             if (isset($_GET["ERROR"]) && $_GET["ERROR"] == 1) {
-                msg(2,"Falha ao cadastrar usuario. Falha ao conectar com a base de dados. Tente novamente mais tarde.<br>Se o problema persistir, por favor entre em contato com o adminstrador do sistema.");
+                msg(2, "Falha ao cadastrar usuario. Falha ao conectar com a base de dados. Tente novamente mais tarde.<br>Se o problema persistir, por favor entre em contato com o adminstrador do sistema.");
             }
             exit();
         }
         ?>
-
         <input type="text" id="ra" name="ra" value="<?php echo $raAtual ?>" readonly>
-        <input type="text" placeholder="Nome" name="nome" id="nome">
-        <span id="nomeError"><?php if (isset($nomeError)) {
-                                    echo $nomeError;
-                                } ?></span>
         <input type="text" name="cpf" id="cpf" class="inputUser" placeholder="CPF" oninput="maskCPF()">
-        <span id="cpfError"><?php if (isset($cpfError)) {
-                                echo $cpfError;
-                            } ?></span>
+        <input type="text" placeholder="Nome" name="nome" id="nome">
         <select id="genero" name="genero">
-            <option value="">Sexo</option>
+            <option value="">Gênero</option>
             <option value="1">Masculino</option>
             <option value="2">Feminino</option>
             <option value="3">Outro</option>
         </select>
-        <input type="date" placeholder="Data de nascimento" name="dtNasc" id="data">
-        <span id="dtError"><?php if (isset($dtError)) {
-                                echo $dtError;
-                            } ?></span>
         <input type="email" name="email" id="email" placeholder="Email">
-        <span id="emailError"><?php if (isset($emailError)) {
-                                    echo $emailError;
-                                } ?></span>
+        <input type="date" name="dtNasc" id="data">
         <input type="password" placeholder="Código de confirmação" name="senha" id="senha">
-        <span id="passwordError"><?php if (isset($passwordError)) {
-                                        echo $passwordError;
-                                    } ?></span>
-
         <select id="tipo" name="tipo" onchange="validarTipo()">
             <option value="">Selecione o tipo</option>
             <option value="1">Gerente</option>
@@ -79,15 +57,12 @@ if ($db->errorCode == 0) {
             <option value="3">Aluno</option>
         </select>
         <!-- Partes específicas ocultas -->
-        <div id="parteFuncionario" style="display: none">
+        
             <!-- Campos específicos para gerente -->
-            <label for="">Data de Admissão</label>
-            <input type="date" name="dtContrato" id="dtContrato">
-        </div>
-
+            <input type="date" name="dtContrato" id="parteFuncionario" style="display: none">
+        
         <div id="parteAluno" style="display: none">
             <!-- Campos específicos para aluno -->
-            <label for="">Data Matrícula</label>
             <input type="date" name="dtMatricula" id="dtMatricula">
             <select name="idTurma">
                 <option value="">Selecione a turma</option>
@@ -103,28 +78,49 @@ if ($db->errorCode == 0) {
             </select>
         </div>
         <input type="submit" name="submit" id="submit" class="btnCad" value="Cadastrar">
+        <div class="msgN">
+            <span id="nomeError">
+                <?php if (isset($nomeError)) {
+                    echo $nomeError;
+                } ?></span>
 
+            <span id="cpfError"><?php if (isset($cpfError)) {
+                                    echo $cpfError;
+                                } ?></span>
+
+            <span id="dtError"><?php if (isset($dtError)) {
+                                    echo $dtError;
+                                } ?></span>
+
+            <span id="emailError"><?php if (isset($emailError)) {
+                                        echo $emailError;
+                                    } ?></span>
+
+            <span id="passwordError"><?php if (isset($passwordError)) {
+                                            echo $passwordError;
+                                        } ?></span>
+        </div>
 
         <!--Mensagens de erro aqui (preferência: 1 por vez)-->
         <?php
         //Menssagem de falha no Banco
         if (isset($_GET["ERROR"]) && $_GET["ERROR"] == 1) {
-            msg(2,"Falha ao cadastrar usuario. Falha ao conectar com a base de dados. Tente novamente mais tarde.<br>Se o problema persistir, por favor entre em contato com o adminstrador do sistema.");
+            msg(2, "Falha ao cadastrar usuario. Falha ao conectar com a base de dados. Tente novamente mais tarde.<br>Se o problema persistir, por favor entre em contato com o adminstrador do sistema.");
         }
-        
+
         //Menssagem de falha no Banco 
         if (isset($_GET["ERROR"]) && $_GET["ERROR"] == 2) {
-            msg(2,"O cadastro falhou!");
+            msg(2, "O cadastro falhou!");
         }
 
         //Menssagem de falha no Banco 
         if (isset($_GET["ERROR"]) && $_GET["ERROR"] == 10) {
-            msg(2,"O campo tipo deve ser preenchido!");
+            msg(2, "O campo tipo deve ser preenchido!");
         }
 
         //Menssagem de erro geral
         if (isset($_GET["ERROR"]) && $_GET["ERROR"] == null) {
-            msg(2,"Erro desconhecido, por favor entre em contato com o adminstrador do sistema.");
+            msg(2, "Erro desconhecido, por favor entre em contato com o adminstrador do sistema.");
         }
         ?>
 
