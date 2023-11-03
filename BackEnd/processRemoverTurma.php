0a538b5a65acd2f9e147f45dbdd832b54fdea775
Guilherme Araujo <tecgui.informatica@gmail.com>
2023-11-03 11:12:23 -0300

    att remover turmas

--- BackEnd/processRemoverTurma.php
+++ BackEnd/processRemoverTurma.php
@@ -4,12 +4,17 @@ requiredLogin(PERMISSION_GERENTE);
 include_once "conexao.php";
 $idTurma = $_GET["id"];
 
-    $db = new Conexao();
-    $db->executar("DELETE FROM turmas WHERE id = '$idTurma';");
-    $db = $db->executar("SELECT id FROM turmas WHERE id = '$idTurma'",true);
-
-if($db->rowCount() == 0){
-    header("Location: ../Gerente/inicioGerente.php?Sucess=2");
-}else{
+$db = new Conexao();
+$result = $db->executar("SELECT id FROM professor_turma WHERE id_turma = '$idTurma'", true);
+if ($result->rowCount() == 0) {
+    $result = $db->executar("SELECT id FROM alunos WHERE id_turma = '$idTurma'", true);
+    if ($result->rowCount() == 0) {
+        $db->executar("DELETE FROM turmas WHERE id = '$idTurma';");
+        $result = $db->executar("SELECT id FROM turmas WHERE id = '$idTurma'", true);
+        if ($result->rowCount() == 0) {
+            header("Location: ../Cadastrados/Turmas.php?Sucess=2");
+        }
+    }
+} else {
     header("Location: ../Gerente/inicioGerente.php?ERRO=21");
 }
